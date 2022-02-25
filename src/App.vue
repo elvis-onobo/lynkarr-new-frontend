c01b2b6254fb9346b47a655d25341e29d95dd467
Elvis Onobo <elvis.onobo@gmail.com>
2022-02-25 07:50:48 +0100

    Chore: Setup homepage

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import LandingPage from './components/LandingPage.vue'
+</script>
+
+<template>
+  <LandingPage />
+</template>
+
+<style>
+
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'LandingPage',
+  data(){
+    return {
+      date: new Date().getFullYear(),
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="grid place-items-center h-screen bg-gray-600 center-center font-normal bgImg">
+    <div class="grid place-items-center">
+      <div class="m-2 grid place-items-center">
+        <img class="" src="../assets/images/tcn-logono-bg.png" />
+        <p class="text-2xl text-white">Welcome to</p>
+        <p class="text-5xl text-orange-600 font-bold">The Covenant Nation</p>
+        <p class="text-2xl text-white">Analytics Platform</p>
+      </div>
+      <form class="mt-2">
+          <input type="email" name="email" placeholder="E-mail" class="p-2 mr-2 rounded-sm" autofocus/>
+          <input type="password" name="password" class="p-2 mr-2 rounded-sm" placeholder="Password"/>
+          <button class="bg-orange-600 hover:bg-gray-600 rounded-sm p-2 text-white">
+            Login
+          </button>
+      </form>
+      <p class="text-white mt-12">&copy; {{date}}, The Covenant Nation </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bgImg {
+  background: url('../assets/images/tcn.jpg') no-repeat center center
+}
+</style>
