0ae3d396389b334ba30d1367096ed62c67d89151
Elvis Onobo <elvis.onobo@gmail.com>
2022-03-06 21:38:17 +0100

    Feat: add payment types

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,54 +1,78 @@
 
 <template>
-  <div class="container">
-    <div class="grid grid-cols-2 gap-2 font-normal">
-      <div class="place-items-center h-screen v-screen items-center">
-        <p class="text-5xl">Get Your Morrni Working Right</p>
-        <p class="text-2xl">The better way to save.</p>
-      </div>
-      <div class="rounded-lg outline outline-solid outline-1">
-        <p class="text-5xl">Get Your Morrni Working Right</p>
-        <p class="text-2xl">The better way to save.</p>
-      </div>
-    </div>
-
-    <div class='grid grid-cols-3 gap-4 place-items-center m-4'>
-      <div class='p-5 rounded-2xl bg-gray-100'>
-        <div class='p-4 rounded-md'>
-          <fa icon="pen" class='bg-sky-200 p-4 rounded-md' /> 
-          <p class='text-3xl'>Register</p>
+  <div class="grid place-items-center">
+    <div class="container">
+      <div class="grid grid-cols-2 gap-1 font-serif place-items-center h-screen">
+        <div class="place-items-center center-center p-6 m-6">
+          <p class="text-5xl bg-gradient-to-tr to-violet-600 font-bold">Give Your Customers all the Options</p>
+          <p class="text-1xl mt-2">10 thousand businesses use AnyPay to enable their customers make seamless payments.</p>
+          <button class="p-2 bg-purple-600 text-white rounded-md hover:bg-fuchsia-600 mt-2">Register</button>
         </div>
-        <p class='pl-4 pr-4'>
-          Automated Savings Build a dedicated savings faster on your terms automatically or manually.
-        </p>
-        <div class='p-4 rounded-md'>
-          <fa icon="arrow-right" class='bg-pink-200 p-2 rounded-full' /> 
+
+        <div class="place-items-center center-center flex">
+          <div class="rounded-full p-20 bg-purple-600"></div>
+          <div class="rounded-full p-20 bg-pink-600"></div>
         </div>
       </div>
 
-      <div class='p-5 rounded-2xl bg-gray-100'>
-        <div class='p-4 rounded-md'>
-          <fa icon="box" class='bg-sky-200 p-4 rounded-md' /> 
-          <p class='text-3xl'>Save</p>
+
+      <div class="grid place-items-center bg-purple-600 rounded-lg p-4 text-white v-screen align-middle m-6">
+        <div>
+          <p class="text-1xl p-4 font-bold font-serif">
+            Pay with any of these and more... from convenient point
+          </p>
         </div>
-        <p class='pl-4 pr-4'>
-          Automated Savings Build a dedicated savings faster on your terms automatically or manually.
-        </p>
-        <div class='p-4 rounded-md'>
-          <fa icon="arrow-right" class='bg-pink-200 p-2 rounded-full' /> 
+        <div class="grid grid-cols-3 gap-2 justify-between content-center">
+          <div>
+            <img src="../assets/images/flutterwave.png" class="h-100" />
+          </div>
+          <div>
+            <img src="../assets/images/paystack.png" class="h-100" />
+          </div>
+          <div>
+            <img src="../assets/images/voguepay.svg" class="h-100" />
+          </div>
         </div>
       </div>
 
-      <div class='p-5 rounded-2xl bg-gray-100'>
-        <div class='p-4 rounded-md'>
-          <fa icon="money-bill" class='bg-sky-200 p-4 rounded-md' /> 
-          <p class='text-3xl'>Earn</p>
+      <div class='grid grid-cols-3 gap-4 place-items-center m-6'>
+        <div class='p-5 rounded-2xl bg-gray-100'>
+          <div class='p-4 rounded-md'>
+            <fa icon="pen" class='bg-purple-200 p-4 rounded-md' /> 
+            <p class='text-3xl'>Register</p>
+          </div>
+          <p class='pl-4 pr-4'>
+            Automated Savings Build a dedicated savings faster on your terms automatically or manually.
+          </p>
+          <div class='p-4 rounded-md'>
+            <fa icon="arrow-right" class='bg-pink-200 p-2 rounded-full' /> 
+          </div>
         </div>
-        <p class='pl-4 pr-4'>
-          Automated Savings Build a dedicated savings faster on your terms automatically or manually.
-        </p>
-        <div class='p-4 rounded-md'>
-          <fa icon="arrow-right" class='bg-pink-200 p-2 rounded-full' /> 
+
+        <div class='p-5 rounded-2xl bg-gray-100'>
+          <div class='p-4 rounded-md'>
+            <fa icon="box" class='bg-purple-200 p-4 rounded-md' /> 
+            <p class='text-3xl'>Save</p>
+          </div>
+          <p class='pl-4 pr-4'>
+            Automated Savings Build a dedicated savings faster on your terms automatically or manually.
+          </p>
+          <div class='p-4 rounded-md'>
+            <fa icon="arrow-right" class='bg-pink-200 p-2 rounded-full' /> 
+          </div>
+        </div>
+
+        <div class='p-5 rounded-2xl bg-gray-100'>
+          <div class='p-4 rounded-md'>
+            <fa icon="money-bill" class='bg-purple-200 p-4 rounded-md' /> 
+            <p class='text-3xl'>Earn</p>
+          </div>
+          <p class='pl-4 pr-4'>
+            Automated Savings Build a dedicated savings faster on your terms automatically or manually.
+          </p>
+          <div class='p-4 rounded-md'>
+            <fa icon="arrow-right" class='bg-pink-200 p-2 rounded-full' /> 
+          </div>
         </div>
       </div>
     </div>
