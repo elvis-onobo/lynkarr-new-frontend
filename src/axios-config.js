feb21d46b7314d564b3dc2a71bea69048a65d7a6
Elvis Onobo <elvis.onobo@gmail.com>
2022-03-06 11:52:53 +0100

    Fix: update url to https

--- src/axios-config.js
+++ src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'http://cross-up.herokuapp.com/' :'http://127.0.0.1:4000/',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://cross-up.herokuapp.com/' :'http://127.0.0.1:4000/',
     headers: {
         Authorization: 'Bearer ' + localStorage.getItem('token'),
     }
