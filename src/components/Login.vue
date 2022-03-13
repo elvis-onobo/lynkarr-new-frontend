0c9aa70f0f69d0d4b618c165494b830d818febae
Elvis Onobo <elvis.onobo@gmail.com>
2022-03-13 07:15:31 +0100

    Feat: Add strip and wise logo

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,8 @@
 
 <template>
-  <div class="grid place-items-center">
+  <div class="grid place-items-center font-serif">
     <div class="container">
-      <div class="grid grid-cols-2 gap-1 font-serif place-items-center h-screen">
+      <div class="grid grid-cols-2 gap-1 place-items-center h-screen">
         <div class="place-items-center center-center p-6 m-6">
           <p class="text-5xl bg-gradient-to-tr to-violet-600 font-bold">Give Your Customers all the Options</p>
           <p class="text-1xl mt-2">
@@ -24,7 +24,7 @@
             Receive payments with these and more... From one convenient stack
           </p>
         </div>
-        <div class="grid grid-cols-3 gap-2 justify-between content-center">
+        <div class="flex gap-2 content-center">
           <div>
             <img src="../assets/images/flutterwave.png" class="w-80" />
           </div>
@@ -32,12 +32,18 @@
             <img src="../assets/images/paystack.png" class="w-80" />
           </div>
           <div>
-            <img src="../assets/images/voguepay.svg" class="w-60" />
+            <img src="../assets/images/voguepay.svg" class="w-52" />
+          </div>
+          <div>
+            <img src="../assets/images/stripe-cropped.svg" class="w-36" />
+          </div>
+          <div>
+            <img src="../assets/images/wise.png" class="w-48" />
           </div>
         </div>
       </div>
 
-      <div class='grid grid-cols-3 gap-4 place-items-center m-6 font-serif'>
+      <div class='grid grid-cols-3 gap-4 place-items-center m-6 '>
         <div class='p-5 rounded-2xl bg-gray-100'>
           <div class='p-4 rounded-md'>
             <fa icon="pen" class='bg-purple-200 p-4 rounded-md' /> 
@@ -78,19 +84,69 @@
         </div>
       </div>
 
-      <div class="grid place-items-center bg-gray-100 rounded-lg p-4 v-screen align-middle m-6">
+      <div class="grid place-items-center bg-pink-100 rounded-lg p-4 v-screen align-middle m-6">
         <div class="grid grid-cols-2 gap-1 content-center">
           <div class="grid place-items-center">
-            <p class="text-2xl p-4 font-serif">
+            <p class="text-2xl p-4 ">
               Join the waitlist
             </p>
           </div>
           <div>
             <form class="">
-              <input text="" class="rounded-md p-2 mr-2" placeholder="E-mail" />
-              <input text="" class="rounded-md p-2 mr-2" placeholder="Name" />
+              <input text="emai;" class="rounded-md p-2 mr-2" placeholder="E-mail" autofocus />
+              <input text="text" class="rounded-md p-2 mr-2" placeholder="Name" />
               <button class="p-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 mt-2">Join</button>
             </form>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid place-items-center justify-between rounded-lg p-4 v-screen align-middle m-6">
+        <div class="grid grid-cols-5 gap-1 content-center p-4">
+          <div>
+            <img src="../assets/images/tcn-logono-bg.png" class="w-24" />
+          </div>          
+          
+          <div class="">
+            <p class="font-bold">Products</p>
+            <a href="http://" >PayBanner</a>
+          </div>
+
+          <div class="">
+            <p class="font-bold">Company</p>
+            <p><a href="http://" >About</a></p>
+            <p><a href="http://" >FAQs</a></p>
+            <p><a href="http://" >Blog</a></p>
+          </div>
+
+          <div class="">
+            <p class="font-bold">Legal</p>
+            <p><a href="http://" >Terms and Conditions</a></p>
+            <p><a href="http://" >Privacy</a></p>
+            <p><a href="http://" >Security</a></p>
+          </div>
+
+          <div class="">
+            <p class="font-bold">Let's Get Social</p>
+            <div class="grid">
+              <fa icon="facebook" /> 
+              <fa icon="instagram" /> 
+              <fa icon="whatsapp" />
+              <div class="text-">
+                <p>16 Ebun Oluwa Street, Shomolu, Lagos</p>
+                <p>service@digitalboss.com</p>
+                <p>+234 803 910 1861</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="grid place-items-center">
+          <div class="grid place-items-center center-center">
+            <p class="p-20">
+              Piggyvest (formerly piggybank.ng) is the leading online savings & investing platform in Nigeria. For over 5 years,
+              our customers have saved and invested billions of Naira that they would normally be tempted to spend.
+            </p>
           </div>
         </div>
       </div>
